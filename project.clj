08f8a067314f013f2dd2a7f2d41c1180df113316
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2014-06-25 16:30:30 -0500

    Created base structure for project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject pharos "0.0.1-SNAPSHOT"
+  :description "Clojure Doumentation Tool"
+  :url "https://github.com/spinningtopsofdoom/pharos"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :source-paths  ["src/clj"])
---
+++ src/clj/pharos/core.clj
@@ -0,0 +1,1 @@
+(ns pharos.core)
