448b8edaa43adf79b9821e12497a6fd6463b1eff
Devin Walters <devinw@gmail.com>
2014-07-23 13:56:19 -0500

    Quick and dirty server for development

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :url "https://github.com/spinningtopsofdoom/pharos"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [ring "1.3.0"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler pharos.core/app
+         :port 3000}
   :source-paths  ["src/clj"])
--- src/clj/pharos/core.clj
+++ src/clj/pharos/core.clj
@@ -1,1 +1,32 @@
-(ns pharos.core)
+(ns pharos.core
+  (:use [hiccup.core])
+  (:require [compojure.core :as c]
+            [compojure.route :as c-route]
+            [compojure.handler :as c-handler]
+            [compojure.response :as c-resp]
+            [ring.adapter.jetty :as r-jetty]
+            [ring.util.response :as r-resp]
+            [ring.middleware.file :as m-file]
+            [ring.middleware.file-info :as m-finfo]
+            [ring.middleware.stacktrace :as m-stacktrace]
+            [ring.middleware.reload :as m-reload]
+            [hiccup.core :refer :all :as h]
+            [hiccup.middleware :refer (wrap-base-url)]))
+
+(defn home-page []
+  (html
+   [:html
+    [:head
+     [:title "Pharos"]]
+    [:body
+     [:h1 "Pharos"]
+     [:p "This is a paragraph about pharos."]]]))
+
+(c/defroutes app-routes
+  (c/GET "/" [] (home-page))
+  (c-route/resources "/")
+  (c-route/not-found "Page not found"))
+
+(def app
+  (-> (c-handler/site app-routes)
+      (wrap-base-url)))
